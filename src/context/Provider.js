875402f16ac6ea77040e549e36cbd102ac1743d3
saint-lag <maiasena.gabriel@gmail.com>
2022-04-26 13:03:46 -0300

    [Hooks]: Trying to implement useEffect, testing it with handleNameFilter

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import fetchPlanets from '../services/fetchStarWarsApi';
 import AppContext from './AppContext';
 
 function Provider({ children }) {
-  const INITIAL_DATA = [];
-  const [data, setData] = useState(INITIAL_DATA);
+  const [data, setData] = useState([]);
   const [filterByName, setFilterByName] = useState({ name: '' });
   const [filterByNumericValues, setFilterByNumericValues] = useState([]);
   const [currentNumericValues, setCurrentNumericValues] = useState({
@@ -28,33 +27,41 @@ function Provider({ children }) {
         .then(({ results }) => setData(results));
     })();
   }, []);
-  useEffect(() => {
-    const handleNameFilter = (results) => (filterByName.name !== ''
-      ? results.filter((result) => result.name
-        .toUpperCase()
+
+  const handleNameFilter = useCallback(() => {
+    setData((filterByName.name !== ''
+      ? data.filter((result) => result.name.toUpperCase()
         .includes(filterByName.name.toUpperCase()))
-      : results);
-    const handleNumericValuesFilter = (results) => results
-      .filter((result) => filterByNumericValues
-        .map((filter) => {
-          const SUBJECT = Number(result[filter.column]);
-          const COMPARATOR = Number(filter.value);
-          switch (filter.comparison) {
-          case 'menor que':
-            return SUBJECT < COMPARATOR;
-          case 'maior que':
-            return SUBJECT > COMPARATOR;
-          case 'igual a':
-            return SUBJECT === COMPARATOR;
-          default:
-            return result;
-          }
-        })
-        .every((bool) => bool));
-    if (data !== INITIAL_DATA) {
-      setData(handleNumericValuesFilter(handleNameFilter(data)));
-    }
-  }, [INITIAL_DATA, data, filterByName, filterByNumericValues]);
+      : data));
+  }, [data, filterByName.name]);
+
+  useEffect(() => {
+    handleNameFilter();
+  }, [handleNameFilter]);
+
+  // useEffect(() => {
+  //   const handleNumericValuesFilter = () => {
+  //     const filteredPlanets = (results) => results
+  //       .filter((result) => filterByNumericValues
+  //         .map((filter) => {
+  //           const SUBJECT = Number(result[filter.column]);
+  //           const COMPARATOR = Number(filter.value);
+  //           switch (filter.comparison) {
+  //           case 'menor que':
+  //             return SUBJECT < COMPARATOR;
+  //           case 'maior que':
+  //             return SUBJECT > COMPARATOR;
+  //           case 'igual a':
+  //             return SUBJECT === COMPARATOR;
+  //           default:
+  //             return result;
+  //           }
+  //         })
+  //         .every((bool) => bool));
+  //     setData(() => filteredPlanets(data));
+  //   };
+  //   handleNumericValuesFilter();
+  // }, [data, filterByNumericValues]);
   return (
     <AppContext.Provider value={ context }>
       {children}
